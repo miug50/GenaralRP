56e35f6b2ecedb96b1d5438c02ee6c9d25841289
hmmac <hmmac@DESKTOP-CJEOPI0>
2020-09-23 21:15:36 +0100

    this is a test to add a system.out

--- testGit/src/testgit/TestGit.java
+++ testGit/src/testgit/TestGit.java
@@ -15,7 +15,7 @@ public class TestGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        System.out.println("hello world. this is a test to make sure NetBeans and Git are getting on");
     }
     
 }
